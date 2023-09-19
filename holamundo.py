db05e1e648b60aae9e405d1a5276abd3cc5a5e46
feerlv <145036053+feerlv@users.noreply.github.com>
2023-09-19 08:50:28 -0600

    Create holamundo.py

---
+++ holamundo.py
@@ -0,0 +1,64 @@
+
+HOLA MUNDO
+print ("hola mundo") 
+
+MAQUINA DE FUNCIONES
+def f (x): 
+  f_x= -2*x+34
+  return f_x 
+
+y= f(10)
+
+print(y)
+
+CODING RUSH
+def calculadora_inutil ():
+  print ("Dime un número que será la base")
+  base = input ()
+  print ("Dame otro número y será tu exponencial") 
+  ex = input ()
+  operacion = int(base) ** 
+  print ("El resultado incorrecto es", operacion)
+
+calculadora_inutil() 
+
+REPASO
+def descuento():
+  print("Dame un precio x")
+  precio = input ()
+  print("Número que será el descuento")
+  descuento = input ()
+  resultado = int (precio) * int (descuento) / 100
+  print("El resultado es", resultado)
+
+descuento()
+
+
+def area_perimetro():
+  print("Dime la base de tu rectángulo")
+  base = input ()
+  print("Ahora dime la altura")
+  altura = input ()
+  resultado = int (base) * int (altura)
+  print("El área es", resultado)
+  resultado = 2 * int (base) + int (altura)
+  print("El perímetro es", resultado)
+
+area_perimetro()
+
+EXAMEN
+def descuento():
+  print("Dime el precio del aluminio")
+  aluminio = input()
+  print ("Ahora el precio de conectores")
+  conectores = input ()
+  print ("Ahora el precio de cajas")
+  cajas = input ()
+  resultado = int (aluminio) + int (conectores) + int (cajas) 
+  print ("El costo final del proyecto es", resultado)
+  resultado = 270 * 5 / 100
+  print("El 5% es", resultado)
+  resultado = 270 - 13.5
+  print("El costo menos el descuento es", resultado)
+
+descuento ()
