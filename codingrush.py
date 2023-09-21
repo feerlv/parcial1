3c5b147de3dc83c690092124d5b30fa44222b9f3
feerlv <145036053+feerlv@users.noreply.github.com>
2023-09-21 08:29:11 -0600

    Create codingrush.py

---
+++ codingrush.py
@@ -0,0 +1,9 @@
+def calculadora_inutil ():
+  print ("Dime un número que será la base")
+  base = input ()
+  print ("Dame otro número y será tu exponencial") 
+  ex = input ()
+  operacion = int(base) ** 
+  print ("El resultado incorrecto es", operacion)
+
+calculadora_inutil() 
